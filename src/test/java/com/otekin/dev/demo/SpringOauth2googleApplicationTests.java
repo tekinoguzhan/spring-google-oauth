e6408ebef4b6536a1df541c3699d93903805afd7
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-10-10 08:46:09 +0300

    Add files via upload

---
+++ src/test/java/com/otekin/dev/demo/SpringOauth2googleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otekin.dev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2googleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
